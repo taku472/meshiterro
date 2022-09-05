77fd369102f23ef5a12edf64f50c929686de19dc
taku472 <taku.3303@icloud.com>
2022-09-05 07:25:09 +0000

    device 8-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  
   root "homes#top"
   devise_for :users
   
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   
   get '/about' => 'homes#about'
   
